e04769e7531dfb3afbfa683175e93fa27583346f
Lowella369 <lh850210@gmail.com>
2020-04-10 15:45:11 -0500

    Adding validation to the script

--- js/main.js
+++ js/main.js
@@ -1,31 +1,37 @@
 function getGIF() {
-	
 	$('#txtGIFSearch').val('');
 	$('#txtNoOfGif').val('');
 
-	$('#btnSearch').click(function () {
+	$('#btnSearch').click(function() {
 		$('#giphy-Holder').empty();
+		var results = 0;
 		var inputKeywords = $('#txtKeywords').val().trim();
 
-		$('#txtGIFSearch').val(inputKeywords);
-		$('#txtNoOfGif').val('12');
+		if(inputKeywords == null || inputKeywords == "") {
+			alert("Please enter a valid keywords to search!");
+			$('#txtGIFSearch').val(inputKeywords);
+			$('#txtNoOfGif').val(0);
+		}
+		else {
+			$('#txtGIFSearch').val(inputKeywords);
+			var urlAPI = 'https://api.giphy.com/v1/gifs/search?api_key=OXTGexsbHE5OVgrtEYNv3WEwKUn4qEmg&q=' + inputKeywords + '&offset=0&rating=G&lang=en';
 
-		var URLAPI = 'https://api.giphy.com/v1/gifs/search?api_key=OXTGexsbHE5OVgrtEYNv3WEwKUn4qEmg&q=' + inputKeywords + '&limit=12&offset=0&rating=G&lang=en';
+			$.ajax({ url: urlAPI}).done(function (response) {
+				results = response.data.length;
+				if (results > 0) {
+					$('#txtNoOfGif').val(results);
 
-		$.ajax({ url: URLAPI }).done(function (response) {
-
-			for (var x = 0; x < response.data.length; x++) {
-
-				var imageURL = response.data[x].images.fixed_height.url;
-
-				$('#giphy-Holder').append('<img src="' + imageURL + '" ></img>');
-			}
-
-		});
-
-		$('#txtKeywords').val('Enter keywords');
-
+					for (var x = 0; x < results; x++)
+					{
+						var imageURL = response.data[x].images.fixed_height.url;
+						$('#giphy-Holder').append('<div class="giphy1"><img class="gif" src="' + imageURL + '" ></img></div>');
+					}
+				}
+				else {
+					alert("No results found! Enter a valid keywords!");
+					$('#txtNoOfGif').val(0);
+				}
+			});
+		}
 	});
 }
-
-
