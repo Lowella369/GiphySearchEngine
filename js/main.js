82d65bafa61307dcce8ba374620799d1bb26f92f
Lowella369 <lh850210@gmail.com>
2020-04-12 15:41:34 -0500

    Adding events to the script

--- js/main.js
+++ js/main.js
@@ -1,37 +1,39 @@
-function getGIF() {
-	$('#txtGIFSearch').val('');
-	$('#txtNoOfGif').val('');
+$(document).ready(function() {
 
-	$('#btnSearch').click(function() {
+	function displayGIF(keywords) {
+		var urLAPI = `https://api.giphy.com/v1/gifs/search?api_key=OXTGexsbHE5OVgrtEYNv3WEwKUn4qEmg&q='${keywords}'&offset=0&rating=G&lang=en`;
+		$.ajax({url: urLAPI}).done(function(response) {
+			results = response.data.length;
+			if(results > 0) {
+				for (var x = 0; x < results; x++)
+				{
+					var imageURL = response.data[x].images.fixed_height.url;
+					var title = response.data[x].title;
+					$('#giphy-Holder').append(`<div class="giphy1"><img class="gif" src=" ${imageURL}" alt="${title}" ></img></div>`);
+				}
+			}
+			else {
+				alert("No results found! Enter a valid keywords!");
+			}
+		});
+	}
+
+	$('#btnSearch').click(getGIF);
+
+	function getGIF(event) {
+		event.preventDefault();
 		$('#giphy-Holder').empty();
 		var results = 0;
-		var inputKeywords = $('#txtKeywords').val().trim();
+		var inputKeywords = encodeURIComponent($('#txtKeywords').val().trim())  
 
 		if(inputKeywords == null || inputKeywords == "") {
 			alert("Please enter a valid keywords to search!");
-			$('#txtGIFSearch').val(inputKeywords);
-			$('#txtNoOfGif').val(0);
 		}
 		else {
-			$('#txtGIFSearch').val(inputKeywords);
-			var urlAPI = 'https://api.giphy.com/v1/gifs/search?api_key=OXTGexsbHE5OVgrtEYNv3WEwKUn4qEmg&q=' + inputKeywords + '&offset=0&rating=G&lang=en';
-
-			$.ajax({ url: urlAPI}).done(function (response) {
-				results = response.data.length;
-				if (results > 0) {
-					$('#txtNoOfGif').val(results);
-
-					for (var x = 0; x < results; x++)
-					{
-						var imageURL = response.data[x].images.fixed_height.url;
-						$('#giphy-Holder').append('<div class="giphy1"><img class="gif" src="' + imageURL + '" ></img></div>');
-					}
-				}
-				else {
-					alert("No results found! Enter a valid keywords!");
-					$('#txtNoOfGif').val(0);
-				}
-			});
+			displayGIF(inputKeywords);
 		}
-	});
-}
+	}
+	displayGIF('butterfly')
+});
+
+
